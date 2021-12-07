be9f939c257663a676a6b5ba1f8b710841fa269d
Kristrún Skúladóttir <kristrunskuladottir@gmail.com>
2021-12-07 08:13:54 +0100

    count fish in fewer lines

--- 6/solution_2.py
+++ 6/solution_2.py
@@ -2,23 +2,19 @@ from typing import List
 
 
 def get_expected_count_of_lantern_fish(fish_stages: List[int], number_of_days: int) -> int:
-    new_stages = [0] * 9  # one 0 for each possible stage
+    new_stages = [0] * 9  # There are 9 possible stages
     for stage in fish_stages:
         new_stages[stage] += 1
 
     for day in range(number_of_days):
-        old_stages = new_stages.copy()
+        old_stages = new_stages
         new_stages = [0] * 9
 
-        new_stages[8] = old_stages[0]
-        new_stages[7] = old_stages[8]
-        new_stages[6] = old_stages[0] + old_stages[7]
-        new_stages[5] = old_stages[6]
-        new_stages[4] = old_stages[5]
-        new_stages[3] = old_stages[4]
-        new_stages[2] = old_stages[3]
-        new_stages[1] = old_stages[2]
-        new_stages[0] = old_stages[1]
+        for i in range(8):
+            new_stages[i] = old_stages[i + 1]
+
+        new_stages[8] += old_stages[0]
+        new_stages[6] += old_stages[0]
 
     return sum(new_stages)
 
